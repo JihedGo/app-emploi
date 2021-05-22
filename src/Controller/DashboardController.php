3503fe98ca15a1f054ac58ece891d0ed02227c25
webdev <jihed.gouay@gmail.com>
2021-05-23 00:28:54 +0100

    user-list menu-list

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -17,4 +17,20 @@ class DashboardController extends AbstractController
             'controller_name' => 'DashboardController',
         ]);
     }
+
+    /**
+     * @Route("/user/list", name="user_list")
+     */
+    public function user_list(): Response
+    {
+        return $this->render('dashboard/user-list.html.twig');
+    }
+
+    /**
+     * @Route("/pubs/list", name="pubs_list")
+     */
+    public function pubs_list(): Response
+    {
+        return new Response('liste des publications');
+    }
 }
